3f650a8855be185bf4958d8b986348989594cb6b
Jill Molloy <jill@designdigital.org>
2014-01-15 03:27:08 +0000

    Title fix

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,4 +1,6 @@
 <header class="main-header">
 	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-	<span class="main-description"><?php bloginfo( 'description' ); ?></span>
+	<span class="main-description">
+    <?php bloginfo( 'description' ); ?>
+  </span>
 </header>
